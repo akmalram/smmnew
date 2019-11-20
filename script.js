d6f4379dcae77325c04f6a2dab7e9c4ed4d54521
Akmal <islamov_akmal@inbox.ru>
2019-11-20 15:57:36 +0500

    asdfasdfasdfadfasdf

--- script.js
+++ script.js
@@ -34,6 +34,27 @@ window.addEventListener('load', () => {
             else swiper.slideNext();
         }
     });
+
+    const modalToggler = (buttonClass, modalClass) => {
+        let btn = document.querySelectorAll(buttonClass),
+            modal = document.querySelector(modalClass),
+            closetBtn = document.querySelector(`${modalClass} .close-btn`),
+            modalBackground = document.querySelector(`${modalClass} .modal-background`);
+
+        btn.forEach(one => {
+            one.addEventListener('click', () => {
+                modal.classList.add('active');
+            });
+        });
+
+        [closetBtn, modalBackground].forEach(one => {
+            one.addEventListener('click', () => {
+                modal.classList.remove('active');
+            });
+        });
+    }
+
+    modalToggler('.modal-open-btn', '.register-modal');
 });
 
 const fontAwesomeFreeObserver = new FontFaceObserver('Font Awesome 5 Free');
